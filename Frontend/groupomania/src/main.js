2fcd488b5bc31de551dcab5f1114ad70fdb8a033
kh-issa86 <72387525+kh-issa86@users.noreply.github.com>
2021-07-23 01:16:51 +0200

    login page

--- Frontend/groupomania/src/main.js
+++ Frontend/groupomania/src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).use(router).mount('#app')
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
