8ff0dca6162c2642cf589ddc94b383a7f1c6246b
kh-issa86 <72387525+kh-issa86@users.noreply.github.com>
2021-07-29 01:22:20 +0200

    add morgan package for logging http requests

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,8 @@
 //import packages
 const express = require("express");
-const cors = require('cors');
-const helmet = require('helmet');
+const morgan = require('morgan'); //HTTP request logger middleware for node.js
+const cors = require("cors");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const path = require("path"); // to make path manipulation easier
 
@@ -10,10 +11,14 @@ const userRoutes = require("./routes/user");
 const postsRoutes = require("./routes/posts");
 
 //db
-const { sequelize } = require('./models/index');
+const { sequelize } = require("./models/index");
 
 //Using express
 const app = express();
+
+//Create a new morgan logger middleware
+//function using the tiny format
+app.use(morgan('tiny'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,27 +29,25 @@ app.use(helmet()); // helmet
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome" });
+  res.json({ message: "Welcome" });
 });
 
 //path to images
-app.use('/upload', express.static(path.join(__dirname, 'upload')));
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 //path to user-related routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 //path to feed-related routes
 
-app.use('/api/posts', postsRoutes);
-
+app.use("/api/posts", postsRoutes);
 
 const dbTest = async function () {
-    try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  };
-  dbTest();
-
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+dbTest();
 
 module.exports = app;
